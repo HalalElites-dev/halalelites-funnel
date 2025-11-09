adf2a2a30f5de8c9d3dc6ea3bf1d871b67ca9ec4
Habeeb <hsowemi@rockets.utoledo.edu>
2025-11-09 08:13:36 -0500

    'module not found' fixed - letter cases

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
 import { AcceleratorSection } from "@/components/sections/accelerator";
 import { BookingSection } from "@/components/sections/book";
-import { HeroSection } from "@/components/sections/hero";
-import { PortfolioSection } from "@/components/sections/portfolio";
+import { HeroSection } from "@/components/sections/Hero";
+import { PortfolioSection } from "@/components/sections/Portfolio";
 import { ProcessSection } from "@/components/sections/process";
 import { WhyItMattersSection } from "@/components/sections/why";
 
-
-
 export default function Home() {
   return (
-  <main>
-   <HeroSection />
-   <WhyItMattersSection />
-   <ProcessSection />
-   <PortfolioSection />
-   <BookingSection/>
-   <AcceleratorSection/>
-  </main>
+    <main>
+      <HeroSection />
+      <WhyItMattersSection />
+      <ProcessSection />
+      <PortfolioSection />
+      <BookingSection />
+      <AcceleratorSection />
+    </main>
   );
 }
