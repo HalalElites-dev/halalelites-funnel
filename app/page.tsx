c3761839aaa74a8bf732b7613613edc414f6607b
IbrahimShaikhli <ibrahim.alshaikhli@outlook.com>
2025-11-10 12:01:04 +0400

    name updaet

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { AcceleratorSection } from "@/components/sections/accelerator";
 import { BookingSection } from "@/components/sections/book";
-import { DetailedValueSection, HeroSection } from "@/components/sections/hero";
-import { PortfolioSection } from "@/components/sections/portfolio";
+import { DetailedValueSection, HeroSection } from "@/components/sections/heroSection";
+import { PortfolioSection } from "@/components/sections/portfolioSection";
 import { ProcessSection } from "@/components/sections/process";
 import { WhyItMattersSection } from "@/components/sections/why";
 
